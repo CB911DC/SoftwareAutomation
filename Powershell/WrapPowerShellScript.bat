e4afae5c82d023c4fd8010ea98a2d9708eaccbb0
Wallner Manfred <schusterfredl@mwallner.net>
2017-01-26 11:05:36 +0100

    .bat wrapper for PowerShell scripts

---
+++ Powershell/WrapPowerShellScript.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+set psscript="path_to_your_script"
+echo ==================================================
+echo ============= WRAP POWERSHELL SCRIPT =============
+echo ==================================================
+
+echo calling %psscript% ksbuild with args %*
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%psscript%' %*"
+
+echo ==================================================
+endlocal
