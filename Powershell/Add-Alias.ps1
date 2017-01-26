fabc653bc26b4cce75a75edda59214296784cb00
Wallner Manfred <schusterfredl@mwallner.net>
2017-01-26 10:32:14 +0100

    determine current session from unknown context
    needed when used from chocolatey*.ps1 files

--- Powershell/Add-Alias.ps1
+++ Powershell/Add-Alias.ps1
@@ -9,18 +9,27 @@ $profilepath = switch ($visibility) {
                                       'alluser'   { $PROFILE.AllUsersAllHosts }
                                     }
 
+if (-Not $profilepath) {
+  if ($profilepath -And (-Not $PROFILE)) {
+    Write-Warning "this is awkwark - could not determine your PS session, using fallback (alluser)"
+    $profilepath = Join-Path $env:windir "System32\WindowsPowerShell\v1.0\profile.ps1"
+  } else {
+    Write-Warning "this is awkwark - could not determine your PS session, using fallback (current profile)"
+    $profilepath = $PROFILE
+  }
+}
+
 Write-Output "Add-Alias: $aliasName -> $aliasTarget"
 Write-Output "using profile $profilepath"
 
 if (-Not (Test-Path $profilepath)) {
-  Write-Output "PS profile $profilepath does not exist yet - creating one!"
+  Write-Warning "PS profile $profilepath does not exist yet - creating one!"
   New-Item -Path $profilepath -ItemType File
 }
 
-
 $checkExists = $(Select-String -Path $profilepath -Pattern "New-Alias .*$aliasName .*")
 if ($checkExists) {
-  Write-Output "the alias seems to exist .. skipping"
+  Write-Warning "the alias seems to exist .. skipping"
   Write-Output "$checkExists"
 } else {
   "`nNew-Alias -Name $aliasName -Value $aliasTarget" | Out-File -Append $profilepath -Encoding utf8
