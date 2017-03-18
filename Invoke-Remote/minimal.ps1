690df4fc5642f8f3d14870a9a1f5f059cb12afd2
Manfred Wallner <schusterfredl@mwallner.net>
2017-03-18 22:34:30 +0100

    minimal win pc setup

---
+++ Invoke-Remote/minimal.ps1
@@ -0,0 +1,22 @@
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+Update-ExecutionPolicy RemoteSigned
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-UAC
+Disable-GameBarTips
+
+Enable-PSRemoting -Force
+Set-WSManQuickConfig -SkipNetworkProfileCheck -Force
+Set-Item wsman:\localhost\client\trustedhosts * -Force
+Restart-Service WinRM -Force
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+cinstm notepadplusplus
+cinstm 7zip
+cinstm firefox
