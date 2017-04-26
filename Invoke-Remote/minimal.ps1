85d6773bac330c19252a14f828f167d85ef56fcf
Manfred Wallner <mwallner@users.noreply.github.com>
2017-04-26 23:17:33 +0200

    Update minimal.ps1

--- Invoke-Remote/minimal.ps1
+++ Invoke-Remote/minimal.ps1
@@ -4,21 +4,11 @@ $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
 Update-ExecutionPolicy RemoteSigned
-Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 Enable-RemoteDesktop
-Disable-InternetExplorerESC
 Disable-UAC
-Disable-GameBarTips
 
 # required for usage of 'Invoke-Remote'
 Enable-PSRemoting -Force
 Set-WSManQuickConfig -SkipNetworkProfileCheck -Force
 Set-Item wsman:\localhost\client\trustedhosts * -Force
 Restart-Service WinRM -Force
-
-if (Test-PendingReboot) { Invoke-Reboot }
-
-# basically necessary for any PC ;-)
-cinst notepadplusplus
-cinst 7zip
-cinst firefox
